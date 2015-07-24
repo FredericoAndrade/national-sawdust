3822fcac1ab06ef2c8d8636ccf327f8090e7df68
Freddie Andrade <faandrade@gmail.com>
2015-07-24 17:12:59 -0400

    thumbnail categories

--- search.php
+++ search.php
@@ -7,7 +7,8 @@
 		<h1 class="section-title">Showing results for '<?php the_search_query(); ?>'</h1>
 		<ul class="posts">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<?php $event_thumbnail = wp_get_attachment_image_src(  get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' ); ?>
+			<?php $event_thumbnail = wp_get_attachment_image_src(  get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' );
+			$post_categories = get_the_category( get_the_ID() ); ?>
 			<!-- markup to be considered here-->
 			<li style="background-image:url('<?php echo $event_thumbnail[0]; ?>');" class="related-post <?php if( has_post_thumbnail() ) : ?>has-image <?php else : ?> no-image<?php endif; ?>">
 				<a class="event-link" href="<?php the_permalink(); ?>">
@@ -17,7 +18,11 @@
 						<?php echo_time( get_the_ID() ); ?>
 					</p>
 				</a>
-
+				<ul class="thumbnail-categories">
+					<?php foreach ( $post_categories as $category ) : ?>
+			 			<li><a href="<?php echo get_category_link( $category->term_id ); ?>"><?php echo $category->slug; ?></a></li>
+					<?php endforeach; ?>
+				</ul>
 				<section class="buy-links">
 					<?php echo_purchase_link( get_the_ID(), 'buy-link' ); ?>
 					<?php echo_members_link( get_the_ID(), 'members-link' ); ?>
