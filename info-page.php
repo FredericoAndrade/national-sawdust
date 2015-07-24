e18c2426ed171d3c2224ea4ef7f3c3312df4177e
Freddie Andrade <faandrade@gmail.com>
2015-07-24 15:49:18 -0400

    about pages styled

--- info-page.php
+++ info-page.php
@@ -5,16 +5,23 @@
  */
 
 get_header(); ?>
-
-<nav class="sub-header">
-	<?php wp_list_pages('exclude=2,47,49&title_li='); ?>
-</nav>
+<div class="hero-img-wrap">
+  <?php the_post_thumbnail( 'full', array( 'class' => 'hero-img' ) ); ?>
+</div>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<h1><?php the_title(); ?></h1>
-
+  <div class="page">
+    <div class="sub-header">
+      <nav>
+        <?php wp_list_pages('exclude=2,47,49&title_li='); ?>
+      </nav>
+    </div>
+    <h1 class="section-title"><?php the_title(); ?></h1>
+    <article>
 	<?php the_content(''); ?>
+    </article>
+  </div>
 
 <?php endwhile; endif; ?>
 
--- page.php
+++ page.php
@@ -2,9 +2,15 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<h1><?php the_title(); ?></h1>
+  <div class="hero-img-wrap">
+      <?php the_post_thumbnail( 'full', array( 'class' => 'hero-img' ) ); ?>
+    </div>
+
+  <div class="page">
+	<h1 class="section-title"><?php the_title(); ?></h1>
 
 	<?php the_content(''); ?>
+  </div>
 
 <?php endwhile; endif; ?>
 
