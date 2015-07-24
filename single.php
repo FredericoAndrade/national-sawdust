25bc213603997ef645da5a8d87bdff621ee171cf
Freddie Andrade <faandrade@gmail.com>
2015-07-24 12:08:12 -0400

    single

--- single.php
+++ single.php
@@ -17,6 +17,7 @@
 						<span class="date"><?php the_date('d F, Y'); ?></span>
 					</h1>
 					<?php echo_purchase_link( get_the_ID(), 'buy-link' ); ?>
+					<?php echo_members_link( get_the_ID(), 'members-link' ); ?>
 					<article>
 						<?php the_content(); ?>
 					</article>
@@ -25,8 +26,6 @@
 						<?php wp_list_categories( 'exclude=2&title_li=' ); ?>
 					</ul>
 					<p class="date"><?php the_date('m/d/Y'); ?></p>
-					<?php echo_purchase_link( get_the_ID(), 'buy-link' ); ?>
-					<?php echo_members_link( get_the_ID(), 'members-link' ); ?>
 				</div>
 			</div>
 			<div class="archive">
@@ -47,25 +46,18 @@
 		    			$related_args = array( 'category' => $cat_id, 'posts_per_page' =>  -1);
 		    			$related_posts = get_posts( $related_args );
 		    			foreach( $related_posts as $post ) : ?>
-
-				<li class="<?php echo $post->post_name; ?> related-post">
-					<a href="<?php the_permalink(); ?>">
-						<?php the_post_thumbnail( 'medium', array( 'class' => 'event-thumb' ) ); ?>
-						<p class="thumb-title"><?php the_title(); ?></p>
-						<p class="thumb-meta">
-							<div class="thumb-date left">
-								<p class="thumb-date">
-									<?php $date = new DateTime( get_field('event_date') ); echo $date->format('d F, Y'); ?>
-								</p>
+					<li class="<?php echo $post->post_name; ?> related-post">
+						<a class="event-link" href="<?php the_permalink(); ?>">
+							<?php the_post_thumbnail( 'medium', array( 'class' => 'event-thumb' ) ); ?>
+							<h1><?php the_title(); ?></h1>
+							<p class="thumb-date">
+								<?php $date = new DateTime( get_field('event_date') ); echo $date->format('d F, Y'); ?>
 								<?php echo_time( get_the_ID() ); ?>
-							</div>
-						</p>
-					</a>
-					<div class="thumb-purchase left">
+							</p>
+						</a>
 						<?php echo_purchase_link( get_the_ID(), 'buy-link' ); ?>
 						<?php echo_members_link( get_the_ID(), 'members-link' ); ?>
-					</div>
-				</li>
+					</li>
 
 		    	<?php endforeach; endforeach; ?>
 		    	</ul>
